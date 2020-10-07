484217e368ffd6875e27d4c65f44e8878c36c37b
Ivan Galaviz <ivan.galaviz@wizeline.com>
2020-10-07 03:37:18 -0500

    Fix log in glitch

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -3,10 +3,11 @@ import { Route, Redirect } from "react-router-dom";
 
 import { storage } from "utils/storage";
 import { AUTH_STORAGE_KEY } from "utils/constants";
+import { useAuth } from "providers/Auth";
 
 const ProtectedRoute = (props: any) => {
-  // No idea why useAuth() doesn't return the correct value only here...
-  const isLoggedIn = Boolean(storage.get(AUTH_STORAGE_KEY));
+  const isLoggedIn =
+    useAuth().isLoggedIn || Boolean(storage.get(AUTH_STORAGE_KEY));
 
   return isLoggedIn ? <Route {...props} /> : <Redirect to="/not-found" />;
 };
